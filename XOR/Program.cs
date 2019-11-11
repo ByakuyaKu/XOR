7241a51430cc2317adaaf771547a21f03b04958c
Toga <rodionphilippov@gmail.com>
2019-11-11 04:06:02 +0400

    done

---
+++ XOR/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace XOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XOR x = new XOR();
+
+            string path = @"D:\GERASKIN\XOR\text.txt";
+            var inputText = "";
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    inputText = sr.ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Text: " + inputText);
+
+            Console.Write("Enter Key: ");
+            var password = Console.ReadLine();
+
+            var encryptedText = x.Encrypt(inputText, password);
+
+            Console.WriteLine("Result of encryption " + encryptedText);
+
+            Console.WriteLine("Result of decryption " + x.Decrypt(encryptedText, password));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ XOR/XOR.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XOR
+{
+    public class XOR
+    {
+        private string GetRepeatKey(string s, int n)
+        {
+            var r = s;
+            while (r.Length < n)
+            {
+                r += r;
+            }
+
+            return r.Substring(0, n);
+        }
+
+        private string Cipher(string text, string secretKey)
+        {
+            var currentKey = GetRepeatKey(secretKey, text.Length);
+            var res = "";
+
+            //var ByteText = text.ToCharArray().Select(i => Convert.ToString(i, 2));
+            //var BytecurrentKey = currentKey.ToCharArray().Select(i => Convert.ToString(i, 2));
+
+
+            for (var i = 0; i < text.Count(); i++)
+            {
+                res +=Convert.ToChar((text[i] ^ currentKey[i])).ToString();
+            }
+
+            return res;
+        }
+
+        public string Encrypt(string plainText, string password)
+        => Cipher(plainText, password);
+
+        public string Decrypt(string encryptedText, string password)
+            => Cipher(encryptedText, password);
+
+    }
+}
